29135c72ffbea645d2bac0faad2ba78c8c9948be
AlexandrMazurov <sasha.drozd-97@yandex.ru>
2020-07-01 09:16:42 +0300

    Initial Commit

---
+++ NewsAggregator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAggregator
+//
+//  Created by Александр on 7/1/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
